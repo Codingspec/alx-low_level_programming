2932d44ddd2de79e63dea692389fcfc8f09ce963
Codingspec <mirembesholah78@gmail.com>
2023-02-16 09:37:00 -0800

    territory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
+ * main - Entry point
+ *
+ * Description: "A program that prints the size of various computer types"
+ *
  * Return 0 (Success)
  */
+
 int main(void)
 {
 	char a;
